96b9a1147ebc7ef37143a0370f146fcf70b61b02
Judson Weissert <jweissert@softwriters.com>
2017-08-02 09:10:37 -0400

    Use window.postMessage() to send data to RN
    The react-native WebView supports the onMessage attribute since
    react-native 0.37. This commit uses the postMessage() mechanism to
    transfer data from the WebView to react-native.
    
    This is intended to fix the "Not allowed to navigate top frame to
    data URL" error reported from newer versions of Chromium.

--- index.js
+++ index.js
@@ -45,58 +45,22 @@ class SignaturePad extends Component {
     this.source = {html}; //We don't use WebView's injectedJavaScript because on Android, the WebView re-injects the JavaScript upon every url change. Given that we use url changes to communicate signature changes to the React Native app, the JS is re-injected every time a stroke is drawn.
   }
 
-  _onNavigationChange = (args) => {
-    this._parseMessageFromWebViewNavigationChange(unescape(args.url));
-  };
-
-  _parseMessageFromWebViewNavigationChange = (newUrl) => {
-    //Example input:
-    //applewebdata://4985ECDA-4C2B-4E37-87ED-0070D14EB985#executeFunction=jsError&arguments=%7B%22message%22:%22ReferenceError:%20Can't%20find%20variable:%20WHADDUP%22,%22url%22:%22applewebdata://4985ECDA-4C2B-4E37-87ED-0070D14EB985%22,%22line%22:340,%22column%22:10%7D"
-    //All parameters to the native world are passed via a hash url where every parameter is passed as &[ParameterName]<-[Content]&
-    var hashUrlIndex = newUrl.lastIndexOf('#');
-    if(hashUrlIndex === -1) {
-      return;
-    }
-
-    var hashUrl = newUrl.substring(hashUrlIndex);
-    hashUrl = decodeURIComponent(hashUrl);
-    var regexFindAllSubmittedParameters = /&(.*?)&/g;
-
-    var parameters = {};
-    var parameterMatch = regexFindAllSubmittedParameters.exec(hashUrl);
-    if(!parameterMatch) {
-      return;
-    }
-
-    while(parameterMatch) {
-      var parameterPair = parameterMatch[1]; //For example executeFunction=jsError or arguments=...
-
-      var parameterPairSplit = parameterPair.split('<-');
-      if(parameterPairSplit.length === 2) {
-        parameters[parameterPairSplit[0]] = parameterPairSplit[1];
-      }
-
-      parameterMatch = regexFindAllSubmittedParameters.exec(hashUrl);
-    }
-
-    if(!this._attemptToExecuteNativeFunctionFromWebViewMessage(parameters)) {
-      logger.warn({parameters, hashUrl}, 'Received an unknown set of parameters from WebView');
-    }
-  };
-
   _attemptToExecuteNativeFunctionFromWebViewMessage = (message) => {
     if(message.executeFunction && message.arguments) {
-      var parsedArguments = JSON.parse(message.arguments);
-
       var referencedFunction = this['_bridged_' + message.executeFunction];
       if(typeof(referencedFunction) === 'function') {
-        referencedFunction.apply(this, [parsedArguments]);
+        referencedFunction.apply(this, [message.arguments]);
         return true;
       }
     }
 
     return false;
   };
+
+  _onMessage = (e) => {
+    const message = JSON.parse(e.nativeEvent.data);
+    this._attemptToExecuteNativeFunctionFromWebViewMessage(message)
+  }
 
   _bridged_jsError = (args) => {
     this.props.onError({details: args});
@@ -121,6 +85,7 @@ class SignaturePad extends Component {
                  onNavigationStateChange={this._onNavigationChange}
                  renderError={this._renderError}
                  renderLoading={this._renderLoading}
+                 onMessage={this._onMessage}
                  source={this.source}
                  javaScriptEnabled={true}
                  style={this.props.style}/>
