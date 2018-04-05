2f7f8298fba93a48880dd41c7fe78f6faba9a2df
Daniel Zhang <zhangyingdong89@gmail.com>
2018-04-05 11:09:42 +1000

    fix: trigger onEnd when user draws outside of canvas to capture signature

--- injectedJavaScript/signaturePad.js
+++ injectedJavaScript/signaturePad.js
@@ -33,6 +33,8 @@ var content = `var SignaturePad = (function (document) {
     this.onEnd = opts.onEnd;
     this.onBegin = opts.onBegin;
 
+    this.pointWasOutOfCanvas = false;
+
     this._canvas = canvas;
     this._ctx = canvas.getContext("2d");
     this.clear();
@@ -144,7 +146,24 @@ var content = `var SignaturePad = (function (document) {
       event.preventDefault();
 
       var touch = event.changedTouches[0];
-      self._strokeUpdate(touch);
+      var rect = self._canvas.getBoundingClientRect();
+      var x = touch.clientX - rect.left;
+      var y = touch.clientY - rect.top;
+
+      var pointOutOfCanvas = (x < 0 || y < 0 || x > rect.width || y > rect.height); // checking if point is outside of canvas
+      if (pointOutOfCanvas) {
+        if (!self.pointWasOutOfCanvas) {
+          // if the previous was inside of canvas and the new point it is outside
+          // call onEnd to capture the signature
+          self.onEnd();
+        }
+        // set the pointWasOutOfCanvas to true to indicate that user has drawn out of canvas
+        self.pointWasOutOfCanvas = true;
+      } else {
+        // if the point is still inside canvas call _stokeUpdate as normal
+        self.pointWasOutOfCanvas = false;
+        self._strokeUpdate(touch);
+      }
     });
 
     document.addEventListener("touchend", function (event) {
@@ -169,12 +188,10 @@ var content = `var SignaturePad = (function (document) {
 
   SignaturePad.prototype._createPoint = function (event) {
     var rect = this._canvas.getBoundingClientRect();
-    var x = event.clientX - rect.left;
-    var y = event.clientY - rect.top;
-    // if point is outside of the canvas, reset x,y back to 0
-    x = x < 0 ? 0 : Math.min(x, rect.width);
-    y = y < 0 ? 0 : Math.min(y, rect.height);
-    var point = new Point(x, y);
+    var point = new Point(
+      event.clientX - rect.left,
+      event.clientY - rect.top
+    );
 
     if(this.translateMouseCoordinates)
       this.translateMouseCoordinates(point);
